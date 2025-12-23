bf675cecb58d32b60b7da5546e0dca7de907f2a7
Ni Nyoman Vika Andini <146094489+vikaandinii@users.noreply.github.com>
2025-12-24 01:20:20 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,40 +1,33 @@
+import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import mlflow
 import mlflow.sklearn
-import os
 
 data_path = os.path.join(os.path.dirname(__file__), "hotelbookings_preprocessing/hotelbookings_preprocessing_automate.csv")
-
 if not os.path.exists(data_path):
     raise FileNotFoundError(f"Dataset tidak ditemukan: {os.path.abspath(data_path)}")
 
 df = pd.read_csv(data_path)
 df.columns = df.columns.str.strip()
 
-if "is_canceled" not in df.columns:
-    raise ValueError("Kolom 'is_canceled' tidak ditemukan di dataset. Cek header CSV!")
-
 X = df.drop(columns=["is_canceled"])
 y = df["is_canceled"]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42, stratify=y
-)
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 model = RandomForestClassifier(random_state=42, n_estimators=100)
 
 mlflow.set_tracking_uri("file:mlruns")
 mlflow.set_experiment("Hotel_Cancellation_Model_Basic")
 mlflow.sklearn.autolog()
 
-with mlflow.start_run():
+with mlflow.start_run(): 
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     print(f"Model accuracy: {acc:.4f}")
 
-mlruns_path = os.path.abspath("MLProject/mlruns")
+mlruns_path = os.path.abspath("mlruns")
 print(f"Training selesai. Artefak MLflow disimpan di: {mlruns_path}")
