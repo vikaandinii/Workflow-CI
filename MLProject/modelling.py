819f0cacfb4a9ad842a9f0bd5f507fb4ae508b09
Ni Nyoman Vika Andini <146094489+vikaandinii@users.noreply.github.com>
2025-12-24 01:57:15 +0800

    Change MLflow run to nested and adjust autolog

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -17,13 +17,14 @@ X = df.drop(columns=["is_canceled"])
 y = df["is_canceled"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+
 model = RandomForestClassifier(random_state=42, n_estimators=100)
 
 mlflow.set_tracking_uri("sqlite:///mlflow.db")
 mlflow.set_experiment("Hotel_Cancellation_Model_Basic")
+mlflow.sklearn.autolog()
 
-with mlflow.start_run():
-    mlflow.sklearn.autolog()  
+with mlflow.start_run(nested=True):
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
